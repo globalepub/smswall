0922511a0644895aaab2f79feaebafe3de679f53
dweez <zedwee@gmail.com>
2013-09-11 16:21:11 +0200

    doctype

--- smswall/index.php
+++ smswall/index.php
@@ -13,7 +13,8 @@ try {
 $theme = (!empty($config)) ? $config['theme'] : 'default';
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
 	<meta name="generator" content="Bug" />
 	<title>SmsTwitterWall</title>
@@ -98,5 +99,3 @@ $theme = (!empty($config)) ? $config['theme'] : 'default';
 
 </body>
 </html>
-
-
